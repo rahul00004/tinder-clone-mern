9ed2b72e5cb1a546af216d3f4fc6feeaccf5796e
rahul00004 <41772039+rahul00004@users.noreply.github.com>
2020-11-08 15:37:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './Header.js'
+
+function App() {
+  return (
+    <div className="app">
+
+     <h1 >hello</h1>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
